816160697f73557c3c59153a8188c78fd3911929
antonMaxtone <anton.maxtone@gmail.com>
2019-01-11 18:09:30 +0200

    add simple carousel

--- src/app/images/images.component.ts
+++ src/app/images/images.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'lec-images',
   templateUrl: './images.component.html',
-  styleUrls: ['./images.component.scss']
+  styleUrls: ['./images.component.scss'],
+  providers: [NgbCarouselConfig]
 })
-export class ImagesComponent implements OnInit {
-
-  constructor() { }
+export class ImagesComponent implements OnInit {images = [1, 2, 3, 4].map(() => `https://picsum.photos/1265/500?random&t=${Math.random()}`);
+  constructor(config: NgbCarouselConfig) {
+    config.interval = 2000;
+    config.wrap = true;
+    config.keyboard = true;
+    config.pauseOnHover = true;
+   }
 
   ngOnInit() {
   }
